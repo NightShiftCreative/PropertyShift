caa8f24d230c614840471b20b3c91ef5b7c15043
RypeCreative <40130800+RypeCreative@users.noreply.github.com>
2018-09-12 14:47:49 -0400

    updates

--- includes/admin-pages.php
+++ includes/admin-pages.php
@@ -18,83 +18,69 @@ function rype_real_estate_register_options() {
 /*-----------------------------------------------------------------------------------*/
 /*  OUTPUT SETTINGS PAGE
 /*-----------------------------------------------------------------------------------*/
-function rype_real_estate_settings_page() { ?>
+function rype_real_estate_settings_page() {
+    $page_name = 'Rype Real Estate';
+    $settings_group = 'rype-real-estate-settings-group';
+    $pages = array();
+    $pages[] = array('slug' => 'rype-real-estate-settings', 'name' => 'Settings', 'active' => 'true');
+    $pages[] = array('slug' => 'rype-real-estate-add-ons', 'name' => 'Add-Ons', 'active' => 'false');
+    $pages[] = array('slug' => 'rype-real-estate-help', 'name' => 'Help', 'active' => 'false');
+    $display_actions = 'true';
+    $content = rype_real_estate_settings_page_content();
+    echo rype_basics_admin_page($page_name, $settings_group, $pages, $display_actions, $content);
+} 
 
-    <div class="wrap">
-        <h1><?php esc_html_e('Rype Real Estate', 'rype-real-estate'); ?></h1>
-
-        <div class="rype-settings">
-
-            <div class="rype-settings-menu-bar rype-settings-header">
-                <div class="rype-settings-nav">
-                    <ul>
-                        <li class="active"><a href="?page=rype-real-estate-settings"><?php esc_html_e('Settings', 'rype-real-estate'); ?></a></li>
-                        <li><a href="?page=rype-real-estate-add-ons"><?php esc_html_e('Add-Ons', 'rype-real-estate'); ?></a></li>
-                        <li><a href="?page=rype-real-estate-help"><?php esc_html_e('Help', 'rype-real-estate'); ?></a></li>
-                    </ul>
-                </div>
-                <div class="rype-settings-actions"><?php submit_button(esc_html__('Save Changes', 'rype-real-estate'), 'admin-button', 'submit', false); ?></div>
-                <div class="clear"></div>
-            </div>
-
-            <div class="rype-settings-content">
-                <h3><?php esc_html_e('Settings', 'rype-real-estate'); ?></h3>  
-                <form method="post" action="options.php">
-                    <?php settings_fields( 'rype-real-estate-settings-group' ); ?>
-                    <?php do_settings_sections( 'rype-real-estate-settings-group' ); ?>
-                </form> 
-            </div>
-
-            <div class="rype-settings-menu-bar rype-settings-footer">
-                <?php
-                $plugin_data = get_plugin_data( __FILE__ );
-                $plugin_version = $plugin_data['Version']; 
-                ?>
-                <div class="rype-settings-version left"><?php esc_html_e('Version', 'rype-real-estate'); ?> <?php echo $plugin_version; ?> | <?php esc_html_e('Made by', 'rype-real-estate'); ?> <a href="http://rypecreative.com/" target="_blank">Rype Creative</a> | <a href="http://rypecreative.com/contact/#theme-support" target="_blank"><?php esc_html_e('Get Support', 'rype-real-estate'); ?></a></div>
-                <div class="rype-settings-actions"><?php submit_button(esc_html__('Save Changes', 'rype-real-estate'), 'admin-button', 'submit', false); ?></div>
-                <div class="clear"></div>
-            </div>
-
-        </div><!-- end rype settings -->
-    </div><!-- end wrap -->
-
-<?php } 
+function rype_real_estate_settings_page_content() {
+    ob_start(); ?>
+    <h3><?php esc_html_e('Settings', 'rype-real-estate'); ?></h3>
+    <?php $output = ob_get_clean();
+    return $output;
+}
 
 
 /*-----------------------------------------------------------------------------------*/
 /*  OUTPUT ADD-ONS PAGE
 /*-----------------------------------------------------------------------------------*/
-function rype_real_estate_add_ons_page() { ?>
-	<div class="wrap">
-        <h1><?php esc_html_e('Rype Real Estate', 'rype-real-estate'); ?></h1>
+function rype_real_estate_add_ons_page() { 
+	$page_name = 'Rype Real Estate';
+    $settings_group = 'ype-real-estate-add-ons-group';
+    $pages = array();
+    $pages[] = array('slug' => 'rype-real-estate-settings', 'name' => 'Settings', 'active' => 'false');
+    $pages[] = array('slug' => 'rype-real-estate-add-ons', 'name' => 'Add-Ons', 'active' => 'true');
+    $pages[] = array('slug' => 'rype-real-estate-help', 'name' => 'Help', 'active' => 'false');
+    $display_actions = 'true';
+    $content = rype_real_estate_add_ons_page_content();
+    echo rype_basics_admin_page($page_name, $settings_group, $pages, $display_actions, $content);
+}
 
-        <h2 class="nav-tab-wrapper">
-		    <a href="?page=rype-real-estate-settings" class="nav-tab"><?php esc_html_e('Settings', 'rype-real-estate'); ?></a>
-		    <a href="?page=rype-real-estate-add-ons" class="nav-tab nav-tab-active"><?php esc_html_e('Add-Ons', 'rype-real-estate'); ?></a>
-		    <a href="?page=rype-real-estate-help" class="nav-tab"><?php esc_html_e('Help', 'rype-real-estate'); ?></a>
-		</h2>
-
-		<form method="post" action="options.php">
-            <?php settings_fields( 'rype-real-estate-add-ons-group' ); ?>
-            <?php do_settings_sections( 'rype-real-estate-add-ons-group' ); ?>
-        </form>
-    </div>
-<?php }
+function rype_real_estate_add_ons_page_content() {
+    ob_start(); ?>
+    <h3><?php esc_html_e('Add-Ons', 'rype-real-estate'); ?></h3>
+    <?php $output = ob_get_clean();
+    return $output;
+}
 
 /*-----------------------------------------------------------------------------------*/
 /*  OUTPUT HELP PAGE
 /*-----------------------------------------------------------------------------------*/
-function rype_real_estate_help_page() { ?>
-	<div class="wrap">
-        <h1><?php esc_html_e('Rype Real Estate', 'rype-real-estate'); ?></h1>
+function rype_real_estate_help_page() { 
+    $page_name = 'Rype Real Estate';
+    $settings_group = null;
+    $pages = array();
+    $pages[] = array('slug' => 'rype-real-estate-settings', 'name' => 'Settings', 'active' => 'false');
+    $pages[] = array('slug' => 'rype-real-estate-add-ons', 'name' => 'Add-Ons', 'active' => 'false');
+    $pages[] = array('slug' => 'rype-real-estate-help', 'name' => 'Help', 'active' => 'true');
+    $display_actions = 'false';
+    $content = rype_real_estate_help_page_content();
+    echo rype_basics_admin_page($page_name, $settings_group, $pages, $display_actions, $content);
+}
 
-        <h2 class="nav-tab-wrapper">
-		    <a href="?page=rype-real-estate-settings" class="nav-tab"><?php esc_html_e('Settings', 'rype-real-estate'); ?></a>
-		    <a href="?page=rype-real-estate-add-ons" class="nav-tab"><?php esc_html_e('Add-Ons', 'rype-real-estate'); ?></a>
-		    <a href="?page=rype-real-estate-help" class="nav-tab nav-tab-active"><?php esc_html_e('Help', 'rype-real-estate'); ?></a>
-		</h2>
-    </div>
-<?php }
+function rype_real_estate_help_page_content() {
+    ob_start(); ?>
+    <h3><?php esc_html_e('Help', 'rype-real-estate'); ?></h3>
+    <?php $output = ob_get_clean();
+    return $output;
+}
 
 
 ?>
