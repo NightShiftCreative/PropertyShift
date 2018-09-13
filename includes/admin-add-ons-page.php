4a1f763b474f2ac4425cf4ecd7df4864f803689f
RypeCreative <40130800+RypeCreative@users.noreply.github.com>
2018-09-12 20:03:00 -0400

    updates

--- includes/admin-add-ons-page.php
+++ includes/admin-add-ons-page.php
@@ -1,5 +1,4 @@
 <?php 
-
 /*-----------------------------------------------------------------------------------*/
 /*  OUTPUT ADD-ONS PAGE STRUCTURE
 /*-----------------------------------------------------------------------------------*/
@@ -23,6 +22,7 @@ function rype_real_estate_add_ons_page_content() {
     ob_start(); ?>
     
     <div class="rype-module-group rype-module-group-basic">
+
         <div class="admin-module">
             <div class="rype-module-header">
                 <div class="rype-module-icon"><img src="" alt="" /></div>
@@ -30,12 +30,31 @@ function rype_real_estate_add_ons_page_content() {
             </div>
             <div class="rype-module-content">
                 <span class="admin-module-note"><?php esc_html_e('Sell more properties by advertising open houses. Add unlimited open houses dates and times to your listings.', 'rype-real-estate'); ?></span>
-                <a href="#" target="_blank" class="view-details rype-meta-item"><?php esc_html_e('Get Add-On', 'rype-real-estate'); ?> </a>
+                <?php if(rype_real_estate_is_paid_plugin_active('rype-open-houses')) { ?>
+                    <a href="#" class="view-details rype-meta-item"><?php esc_html_e('Register License Key', 'rype-real-estate'); ?></a>
+                    <input type="text" name="" placeholder="Enter License Key" />
+                    <input type="email" name="" placeholder="Enter email" />
+                <?php } else { ?>
+                    <a href="#" target="_blank" class="view-details rype-meta-item"><?php esc_html_e('Get Add-On', 'rype-real-estate'); ?> </a>
+                <?php } ?>
             </div>
         </div>
+
         <div class="admin-module coming-soon"><div class="rype-module-content"><i class="fa fa-plus"></i> <span>More Coming Soon...</span></div></div>
     </div>
 
     <?php $output = ob_get_clean();
     return $output;
+}
+
+/*-----------------------------------------------------------------------------------*/
+/* Check if add-on plugin is activated 
+/*-----------------------------------------------------------------------------------*/
+function rype_real_estate_is_paid_plugin_active($add_on_slug) {
+    include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+    if(is_plugin_active($add_on_slug.'/'.$add_on_slug.'.php')) { 
+        return true; 
+    } else { 
+        return false;
+    }
 }
