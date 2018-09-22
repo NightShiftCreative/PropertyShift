01807faf355c766dc08cd0c65273afb68d017c77
RypeCreative <40130800+RypeCreative@users.noreply.github.com>
2018-09-21 23:36:55 -0400

    updates

--- js/dynamic_scripts.php
+++ js/dynamic_scripts.php
@@ -1,4 +1,7 @@
 <?php
+//Get RTL (right to left)
+if(isset($_GET['rtl'])) { $rtl = $_GET['rtl']; } else { $rtl = esc_attr(get_option('rypecore_rtl')); } 
+
 //Get currency options
 $currency_symbol = esc_attr(get_option('rypecore_currency_symbol', '$'));
 $currency_symbol_position = esc_attr(get_option('rypecore_currency_symbol_position', 'before'));
@@ -9,6 +12,7 @@ $currency_decimal_num = esc_attr(get_option('rypecore_num_decimal', '0'));
 $dynamic_script = '';
 
 //OUTPUT VARIABLES FOR USE IN rype-real-estate.js
+$dynamic_script .= "var rtl = '{$rtl}';";
 $dynamic_script .= "var currency_symbol = '{$currency_symbol}';";
 $dynamic_script .= "var currency_symbol_position = '{$currency_symbol_position}';";
 $dynamic_script .= "var currency_thousand = '{$currency_thousand}';";
