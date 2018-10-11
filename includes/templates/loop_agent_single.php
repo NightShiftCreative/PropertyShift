fb06140ebe47246b28704fa960ab6e023614c157
RypeCreative <40130800+RypeCreative@users.noreply.github.com>
2018-10-11 16:19:54 -0400

    updates

--- includes/templates/loop_agent_single.php
+++ includes/templates/loop_agent_single.php
@@ -58,49 +58,46 @@
                     <!--******************************************************-->
                     <!-- OVERVIEW -->
                     <!--******************************************************-->
-                   
-                		<div class="agent-single-item agent-<?php echo esc_attr($slug); ?>">
+                	<div class="agent-single-item agent-<?php echo esc_attr($slug); ?>">
 
-                            <a href="<?php the_permalink(); ?>" class="agent-img">
-                                <?php if(isset($meta_post_count) && $meta_post_count > 0) { ?>
-                                    <div class="button alt button-icon agent-tag agent-assigned"><?php echo rypecore_get_icon($icon_set, 'home'); ?><?php echo esc_attr($meta_post_count); ?> <?php if($meta_post_count <= 1) { esc_html_e('Assigned Property', 'rype-real-estate'); } else { esc_html_e('Assigned Properties', 'rype-real-estate'); } ?></div>
-                                <?php } ?>
-                                <?php if ( has_post_thumbnail() ) {  ?>
-                                    <div class="img-fade"></div>
-                                    <?php the_post_thumbnail('full'); ?>
-                                <?php } else { ?>
-                                    <img src="<?php echo plugins_url( '/rype-real-estate/images/agent-img-default.gif' ); ?>" alt="" />
-                                <?php } ?>
-                            </a>
+                        <a href="<?php the_permalink(); ?>" class="agent-img">
+                            <?php if(isset($meta_post_count) && $meta_post_count > 0) { ?>
+                                <div class="button alt button-icon agent-tag agent-assigned"><?php echo rypecore_get_icon($icon_set, 'home'); ?><?php echo esc_attr($meta_post_count); ?> <?php if($meta_post_count <= 1) { esc_html_e('Assigned Property', 'rype-real-estate'); } else { esc_html_e('Assigned Properties', 'rype-real-estate'); } ?></div>
+                            <?php } ?>
+                            <?php if ( has_post_thumbnail() ) {  ?>
+                                <div class="img-fade"></div>
+                                <?php the_post_thumbnail('full'); ?>
+                            <?php } else { ?>
+                                <img src="<?php echo plugins_url( '/rype-real-estate/images/agent-img-default.gif' ); ?>" alt="" />
+                            <?php } ?>
+                        </a>
 
-                            <div class="agent-content">
-                                <div class="agent-details">
-        	                		<?php if(!empty($agent_title)) { ?><p><span><?php echo esc_attr($agent_title); ?></span><?php echo rypecore_get_icon($icon_set, 'tag'); ?><?php esc_html_e('Title', 'rype-real-estate'); ?>:</p><?php } ?>
-        	                		<?php if(!empty($agent_email)) { ?><p><span><?php echo esc_attr($agent_email); ?></span><?php echo rypecore_get_icon($icon_set, 'envelope', 'envelope', 'mail'); ?><?php esc_html_e('Email', 'rype-real-estate'); ?>:</p><?php } ?>
-        	                		<?php if(!empty($agent_mobile_phone)) { ?><p><span><?php echo esc_attr($agent_mobile_phone); ?></span><?php echo rypecore_get_icon($icon_set, 'phone', 'telephone'); ?><?php esc_html_e('Mobile', 'rype-real-estate'); ?>:</p><?php } ?>
-        	                		<?php if(!empty($agent_office_phone)) { ?><p><span><?php echo esc_attr($agent_office_phone); ?></span><?php echo rypecore_get_icon($icon_set, 'building', 'apartment', 'briefcase'); ?><?php esc_html_e('Office', 'rype-real-estate'); ?>:</p><?php } ?>
-                                </div>
-                                <?php if(in_array('agent_detail_item_contact', $agent_detail_items)) { ?> 
-                                    <div class="button button-icon agent-message right"><?php echo rypecore_get_icon($icon_set, 'envelope'); ?><?php esc_html_e('Message Agent', 'rype-real-estate'); ?></div>
-                                <?php } ?>
-                                <?php if(!empty($agent_fb) || !empty($agent_twitter) || !empty($agent_google) || !empty($agent_linkedin) || !empty($agent_youtube) || !empty($agent_instagram)) { ?>
-                                <div class="center">
-                                    <ul class="social-icons circle">
-                                        <?php if(!empty($agent_fb)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_fb); ?>" target="_blank"><i class="fa fa-facebook"></i></a></li><?php } ?>
-                                        <?php if(!empty($agent_twitter)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_twitter); ?>" target="_blank"><i class="fa fa-twitter"></i></a></li><?php } ?>
-                                        <?php if(!empty($agent_google)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_google); ?>" target="_blank"><i class="fa fa-google-plus"></i></a></li><?php } ?>
-                                        <?php if(!empty($agent_linkedin)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_linkedin); ?>" target="_blank"><i class="fa fa-linkedin"></i></a></li><?php } ?>
-                                        <?php if(!empty($agent_youtube)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_youtube); ?>" target="_blank"><i class="fa fa-youtube"></i></a></li><?php } ?>
-                                        <?php if(!empty($agent_instagram)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_instagram); ?>" target="_blank"><i class="fa fa-instagram"></i></a></li><?php } ?>
-                                    </ul>
-                                </div>
-                                <?php } ?>
+                        <div class="agent-content">
+                            <div class="agent-details">
+        	                	<?php if(!empty($agent_title)) { ?><p><span><?php echo esc_attr($agent_title); ?></span><?php echo rypecore_get_icon($icon_set, 'tag'); ?><?php esc_html_e('Title', 'rype-real-estate'); ?>:</p><?php } ?>
+        	                	<?php if(!empty($agent_email)) { ?><p><span><?php echo esc_attr($agent_email); ?></span><?php echo rypecore_get_icon($icon_set, 'envelope', 'envelope', 'mail'); ?><?php esc_html_e('Email', 'rype-real-estate'); ?>:</p><?php } ?>
+        	                	<?php if(!empty($agent_mobile_phone)) { ?><p><span><?php echo esc_attr($agent_mobile_phone); ?></span><?php echo rypecore_get_icon($icon_set, 'phone', 'telephone'); ?><?php esc_html_e('Mobile', 'rype-real-estate'); ?>:</p><?php } ?>
+        	                	<?php if(!empty($agent_office_phone)) { ?><p><span><?php echo esc_attr($agent_office_phone); ?></span><?php echo rypecore_get_icon($icon_set, 'building', 'apartment', 'briefcase'); ?><?php esc_html_e('Office', 'rype-real-estate'); ?>:</p><?php } ?>
                             </div>
+                            <?php if(in_array('agent_detail_item_contact', $agent_detail_items)) { ?> 
+                                <div class="button button-icon agent-message right"><?php echo rypecore_get_icon($icon_set, 'envelope'); ?><?php esc_html_e('Message Agent', 'rype-real-estate'); ?></div>
+                            <?php } ?>
+                            <?php if(!empty($agent_fb) || !empty($agent_twitter) || !empty($agent_google) || !empty($agent_linkedin) || !empty($agent_youtube) || !empty($agent_instagram)) { ?>
+                            <div class="center">
+                                <ul class="social-icons circle">
+                                    <?php if(!empty($agent_fb)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_fb); ?>" target="_blank"><i class="fa fa-facebook"></i></a></li><?php } ?>
+                                    <?php if(!empty($agent_twitter)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_twitter); ?>" target="_blank"><i class="fa fa-twitter"></i></a></li><?php } ?>
+                                    <?php if(!empty($agent_google)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_google); ?>" target="_blank"><i class="fa fa-google-plus"></i></a></li><?php } ?>
+                                    <?php if(!empty($agent_linkedin)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_linkedin); ?>" target="_blank"><i class="fa fa-linkedin"></i></a></li><?php } ?>
+                                    <?php if(!empty($agent_youtube)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_youtube); ?>" target="_blank"><i class="fa fa-youtube"></i></a></li><?php } ?>
+                                    <?php if(!empty($agent_instagram)) { ?><li class="agent-footer-item"><a href="<?php echo esc_url($agent_instagram); ?>" target="_blank"><i class="fa fa-instagram"></i></a></li><?php } ?>
+                                </ul>
+                            </div>
+                            <?php } ?>
+                        </div>
 
-                            <div class="clear"></div>
-
-	                	</div>
-                    
+                        <div class="clear"></div>
+	                </div>
                 	<?php } ?>
 
                 	<?php if($slug == 'description' && !empty($post->post_content)) { ?>
