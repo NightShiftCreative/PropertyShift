62e088d7a034e0aca48a60886f8e7403b9fd3f2d
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-01-13 19:46:17 -0500

    Update submit_property.php

--- includes/templates/submit_property.php
+++ includes/templates/submit_property.php
@@ -1,10 +1,10 @@
 <?php global $current_user, $wp_roles; ?>    
 
-<!-- start user dashboard -->
+<!-- start submit property -->
 <div class="user-dashboard">
 	<?php if(is_user_logged_in()) { 
 		if(function_exists('ns_real_estate_property_submit_form')) { echo ns_real_estate_property_submit_form(); }
 	} else {
         ns_basics_template_loader('alert_not_logged_in.php', null, false);
     } ?>
-</div><!-- end user dashboard -->
+</div><!-- end submit property -->
