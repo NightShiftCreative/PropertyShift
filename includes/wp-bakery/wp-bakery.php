63f559317936cd379b22e1e2e6f5086a1bc4e8f6
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-01-17 16:22:52 -0500

    updates

--- includes/wp-bakery/wp-bakery.php
+++ includes/wp-bakery/wp-bakery.php
@@ -7,18 +7,23 @@
 if(ns_real_estate_is_plugin_active('js_composer/js_composer.php')) {  
 
 	/** LIST PROPERTIES **/
+	$num_properties_per_page = esc_attr(get_option('ns_num_properties_per_page', 12));
+
 	add_action('vc_before_init', 'ns_list_properties_callback');
 	function ns_list_properties_callback() {
 		vc_map(array(
 			'name' => esc_html__( 'List Properties', 'ns-real-estate' ),
 			'base' => 'ns_list_properties',
+			'description' => esc_html__( 'Display your property listings', 'ns-real-estate' ),
+			'icon' => plugins_url('/ns-real-estate/images/icon-real-estate.svg'),
 			'class' => '',
-			'category' => 'Content',
+			'category' => 'NS Real Estate',
 			'params' => array(
 				array(
 					'type' => 'textfield',
-					'heading' => 'Test',
-					'param_name' => 'test',
+					'heading' => esc_html__( 'Show Posts', 'ns-real-estate' ),
+					'param_name' => 'show_posts',
+					'value' => $num_properties_per_page,
 				),
 			),
 		));
