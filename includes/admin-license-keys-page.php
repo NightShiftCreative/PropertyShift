e702058a9571c43c43075867ce8a2bb18848e524
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-02-01 17:08:48 -0500

    Update admin-license-keys-page.php

--- includes/admin-license-keys-page.php
+++ includes/admin-license-keys-page.php
@@ -14,14 +14,7 @@ function ns_real_estate_license_keys_page() {
     $ajax = false;
     $icon = plugins_url('/ns-real-estate/images/icon-real-estate.svg');
     echo ns_basics_admin_page($page_name, $settings_group, $pages, $display_actions, $content, $content_classl, $content_nav, $alerts, $ajax, $icon);
-} 
-
-/*-----------------------------------------------------------------------------------*/
-/*  UPDATE LICENSE KEY STATUS
-/*  - fires only when settings are saved
-/*  - processed in NS Basics plugin (ns-basics/includes/license-keys.php)
-/*-----------------------------------------------------------------------------------*/
-add_action( 'update_option_ns_real_estate_open_houses_license', 'ns_basics_activate_license_key', 10, 3 );
+}
 
 /*-----------------------------------------------------------------------------------*/
 /*  OUTPUT PAGE CONTENT
