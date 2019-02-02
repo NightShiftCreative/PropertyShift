2a18004a440e892e5e40a63a3fba8be3b9c2f63c
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-02-02 12:45:08 -0500

    Update admin-add-ons-page.php

--- includes/admin-add-ons-page.php
+++ includes/admin-add-ons-page.php
@@ -20,30 +20,8 @@ function ns_real_estate_add_ons_page() {
 /*  OUTPUT ADD-ONS PAGE CONTENT
 /*-----------------------------------------------------------------------------------*/
 function ns_real_estate_add_ons_page_content() {
-    ob_start(); ?>
+    ob_start();
 
-    <?php 
-    $open_houses_license_default = array('slug' => 'ns-open-houses', 'key' => null, 'email' => null, 'registered' => false, 'error' => null);
-    $open_houses_license = get_option('ns_real_estate_open_houses_license', $open_houses_license_default);
-    ?>
-    
-    <div class="ns-module-group ns-module-group-real-estate">
-        <div class="admin-module">
-            <div class="ns-module-header">
-                <div class="ns-module-icon"><img src="<?php echo plugins_url('/ns-basics/images/icon-post-sharing.svg'); ?>" alt="" /></div>
-                <?php echo ns_basics_get_license_status($open_houses_license, '#', '?page=ns-real-estate-license-keys', 'true'); ?>
-                <h4><?php esc_html_e('Open Houses', 'ns-real-estate'); ?></h4>
-            </div>
-            <div class="ns-module-content">
-                <span class="admin-module-note"><?php esc_html_e('Sell more properties by advertising open houses. Add unlimited open houses dates and times to your listings.', 'ns-real-estate'); ?></span>
-                <a href="#" target="_blank" class="ns-meta-item"><?php esc_html_e('View Details', 'ns-real-estate'); ?> </a>
-            </div>
-        </div>
-
-        <div class="clear"></div>
-    </div>
-
-    <?php 
     $raw_addons = wp_remote_get(
         constant('NS_SHOP_URL').'/plugins/ns-real-estate/add-ons/',
         array(
