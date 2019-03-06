4d14cc4de2a34f1e77f0dcfbf764f196bb95c86f
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-03-06 01:24:52 -0500

    Update admin-license-keys-page.php

--- includes/admin-license-keys-page.php
+++ includes/admin-license-keys-page.php
@@ -34,106 +34,129 @@ function ns_real_estate_license_keys_page_content() {
 }
 
 /*-----------------------------------------------------------------------------------*/
-/*  ACTIVATE LICENSE
-/*  Parameters sent from add-on plugin file
+/*  Retrieve License Key And/or Option Name
 /*-----------------------------------------------------------------------------------*/
-function ns_real_estate_activate_license($license, $status_name, $item_id) {
+function ns_real_estate_get_license($item_id) {
+    $license = array();
+    $license['key_name'] = 'ns_'.$item_id.'_license_key';
+    $license['key'] = trim(get_option('ns_'.$item_id.'_license_key'));
+    $license['status_name'] = 'ns_'.$item_id.'_license_status';
+    $license['status'] = get_option('ns_'.$item_id.'_license_status');
+    return $license;
+}
 
-    $api_params = array(
-        'edd_action' => 'activate_license',
-        'license'    => $license,
-        'item_id'  => $item_id, // the name of our product in EDD
-        'url'      => home_url()
-    );
+/*-----------------------------------------------------------------------------------*/
+/*  ACTIVATE LICENSE
+/*-----------------------------------------------------------------------------------*/
+function ns_real_estate_activate_license() {
+    if(isset($_POST['ns_real_estate_activate_license']) && !empty($_POST['ns_real_estate_activate_license'])) {
 
-    $response = wp_remote_post( NS_SHOP_URL, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+        $item_id = $_POST['ns_real_estate_activate_license'];
+        $license = ns_real_estate_get_license($item_id);
 
-    if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
-        if ( is_wp_error( $response ) ) {
-            $message = $response->get_error_message();
+        $api_params = array(
+            'edd_action' => 'activate_license',
+            'license'    => $license['key'],
+            'item_id'  => $item_id, // the name of our product in EDD
+            'url'      => home_url()
+        );
+
+        $response = wp_remote_post( NS_SHOP_URL, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+
+        if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+            if ( is_wp_error( $response ) ) {
+                $message = $response->get_error_message();
+            } else {
+                $message = __( 'An error occurred, please try again.', 'ns-real-estate' );
+            }
         } else {
-            $message = __( 'An error occurred, please try again.', 'ns-real-estate' );
-        }
-    } else {
 
-        $license_data = json_decode( wp_remote_retrieve_body( $response ) );
+            $license_data = json_decode( wp_remote_retrieve_body( $response ) );
 
-        if(false === $license_data->success ) {
-            switch( $license_data->error ) {
-                case 'expired' :
-                    $message = sprintf(
-                         __( 'Your license key expired on %s.' ),
-                        date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
-                    );
-                    break;
-                case 'disabled' :
-                case 'revoked' :
-                    $message = __( 'Your license key has been disabled.', 'ns-real-estate' );
-                    break;
-                case 'missing' :
-                    $message = __( 'Invalid license.', 'ns-real-estate' );
-                    break;
-                case 'invalid' :
-                case 'site_inactive' :
-                    $message = __( 'Your license is not active for this URL.', 'ns-real-estate' );
-                    break;
-                case 'item_name_mismatch' :
-                    $message = sprintf( __( 'This appears to be an invalid license key for %s.' ), 'NS Open Houses' );
-                    break;
-                case 'no_activations_left':
-                    $message = __( 'Your license key has reached its activation limit.', 'ns-real-estate' );
-                    break;
-                default :
-                    $message = __( 'An error occurred, please try again.', 'ns-real-estate' );
-                    break;
+            if(false === $license_data->success ) {
+                switch( $license_data->error ) {
+                    case 'expired' :
+                        $message = sprintf(
+                             __( 'Your license key expired on %s.' ),
+                            date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
+                        );
+                        break;
+                    case 'disabled' :
+                    case 'revoked' :
+                        $message = __( 'Your license key has been disabled.', 'ns-real-estate' );
+                        break;
+                    case 'missing' :
+                        $message = __( 'Invalid license.', 'ns-real-estate' );
+                        break;
+                    case 'invalid' :
+                    case 'site_inactive' :
+                        $message = __( 'Your license is not active for this URL.', 'ns-real-estate' );
+                        break;
+                    case 'item_name_mismatch' :
+                        $message = sprintf( __( 'This appears to be an invalid license key for %s.' ), 'NS Open Houses' );
+                        break;
+                    case 'no_activations_left':
+                        $message = __( 'Your license key has reached its activation limit.', 'ns-real-estate' );
+                        break;
+                    default :
+                        $message = __( 'An error occurred, please try again.', 'ns-real-estate' );
+                        break;
+                }
             }
         }
-    }
 
-    if(!empty($message)) {
-        $base_url = admin_url( 'admin.php?page=' . NS_REAL_ESTATE_LICENSE_PAGE );
-        $redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
-        wp_redirect( $redirect );
+        if(!empty($message)) {
+            $base_url = admin_url( 'admin.php?page=' . NS_REAL_ESTATE_LICENSE_PAGE );
+            $redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
+            wp_redirect( $redirect );
+            exit();
+        }
+
+        update_option('ns_'.$item_id.'_license_status', $license_data->license);
+        wp_redirect( admin_url( 'admin.php?page=' . NS_REAL_ESTATE_LICENSE_PAGE ) );
         exit();
     }
-
-    update_option($status_name, $license_data->license);
-    wp_redirect( admin_url( 'admin.php?page=' . NS_REAL_ESTATE_LICENSE_PAGE ) );
-    exit();
 }
+add_action('admin_init', 'ns_real_estate_activate_license');
 
 /*-----------------------------------------------------------------------------------*/
 /*  DEACTIVATE LICENSE
-/*  Parameters sent from add-on plugin file
 /*-----------------------------------------------------------------------------------*/
-function ns_real_estate_deactivate_license($license, $status_name, $item_id) {
-    $api_params = array(
-        'edd_action' => 'deactivate_license',
-        'license'    => $license,
-        'item_id'  => $item_id, // the name of our product in EDD
-        'url'      => home_url()
-    );
+function ns_real_estate_deactivate_license() {
+    if(isset($_POST['ns_real_estate_deactivate_license']) && !empty($_POST['ns_real_estate_deactivate_license'])) {
 
-    $response = wp_remote_post( NS_SHOP_URL, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+        $item_id = $_POST['ns_real_estate_deactivate_license'];
+        $license = ns_real_estate_get_license($item_id);
 
-    if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+        $api_params = array(
+            'edd_action' => 'deactivate_license',
+            'license'    => $license['key'],
+            'item_id'  => $item_id, // the name of our product in EDD
+            'url'      => home_url()
+        );
 
-        if ( is_wp_error( $response ) ) {
-            $message = $response->get_error_message();
-        } else {
-            $message = __( 'An error occurred, please try again.' );
+        $response = wp_remote_post( NS_SHOP_URL, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+
+        if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+
+            if ( is_wp_error( $response ) ) {
+                $message = $response->get_error_message();
+            } else {
+                $message = __( 'An error occurred, please try again.' );
+            }
+
+            $base_url = admin_url( 'admin.php?page=' . NS_REAL_ESTATE_LICENSE_PAGE );
+            $redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
+            wp_redirect( $redirect );
+            exit();
         }
 
-        $base_url = admin_url( 'admin.php?page=' . NS_REAL_ESTATE_LICENSE_PAGE );
-        $redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
-        wp_redirect( $redirect );
+        delete_option($license['status_name']);
+        wp_redirect( admin_url( 'admin.php?page=' . NS_REAL_ESTATE_LICENSE_PAGE) );
         exit();
     }
-
-    delete_option($status_name);
-    wp_redirect( admin_url( 'admin.php?page=' . NS_REAL_ESTATE_LICENSE_PAGE ) );
-    exit();
 }
+add_action('admin_init', 'ns_real_estate_deactivate_license');
 
 /*-----------------------------------------------------------------------------------*/
 /*  Catch activation errors and display
@@ -155,5 +178,63 @@ function ns_real_estate_license_admin_notices() {
     }
 }
 add_action( 'admin_notices', 'ns_real_estate_license_admin_notices' );
+
+/*-----------------------------------------------------------------------------------*/
+/*  Output license key form
+/*-----------------------------------------------------------------------------------*/
+function ns_real_estate_output_license_key_form($item_name, $item_id) {
+    
+    $license = ns_real_estate_get_license($item_id);
+    $license_key = $license['key'];
+    $license_status = $license['status'];
+    settings_fields('ns-real-estate-licenses-group'); 
+    ?>
+
+    <div class="ns-accordion ns-license-key">
+        <div class="ns-accordion-header">
+            <i class="fa fa-chevron-right"></i> 
+            <?php echo $item_name; ?> <?php esc_html_e('License', 'ns-real-estate'); ?>
+            <?php if($license_status !== false && $license_status == 'valid' ) { echo '<div class="button admin-button green">'.esc_html__('Active', 'ns-real-estate').'</div>';  }?>
+        </div>
+        <div class="ns-accordion-content">
+
+            <table class="admin-module">
+                <tr>
+                    <td class="admin-module-label">
+                        <label><?php echo esc_html_e('License Key', 'ns-real-estate'); ?></label>
+                        <span class="admin-module-note"><?php esc_html_e('Enter your license key here.', 'ns-real-estate'); ?></span>
+                    </td>
+                    <td class="admin-module-field">
+                        <?php if($license_status == false) { ?>
+                            <input class="license-key-input" name="<?php echo $license['key_name'] ?>" type="text" value="<?php esc_attr_e( $license_key ); ?>" />
+                        <?php } else { ?>
+                            <input value="<?php esc_attr_e( $license_key ); ?>" disabled />
+                            <span class="admin-module-note"><?php echo esc_html_e('Deactivate to modify license key', 'ns-real-estate'); ?></span>
+                        <?php } ?>
+                    </td>
+                </tr>
+            </table>
+
+            <?php if( false !== $license_key && !empty($license_key) ) { ?>
+            <table class="admin-module">
+                <tr>
+                    <td class="admin-module-label"><label><?php echo esc_html_e('License Actions', 'ns-real-estate'); ?></label></td>
+                    <td class="admin-module-field">
+                        <?php if( $license_status !== false && $license_status == 'valid' ) { ?>
+                            <?php wp_nonce_field( 'ns_nonce', 'ns_nonce' ); ?>
+                            <button style="width:150px;" type="submit" class="button-secondary activate-license-button" name="ns_real_estate_deactivate_license" value="<?php echo $item_id; ?>"><?php echo esc_html_e('Deactivate License', 'ns-real-estate'); ?></button>
+                        <?php } else {
+                            wp_nonce_field( 'ns_nonce', 'ns_nonce' ); ?>
+                            <button style="width:150px;" type="submit" class="button-secondary activate-license-button" name="ns_real_estate_activate_license" value="<?php echo $item_id; ?>"><?php echo esc_html_e('Activate License', 'ns-real-estate'); ?></button>
+                        <?php } ?>
+                    </td>
+                </tr>
+            </table>
+            <?php } ?>
+
+        </div>
+    </div>
+
+<?php }
 
 ?>
