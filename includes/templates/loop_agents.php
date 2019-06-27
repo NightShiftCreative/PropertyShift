f3c9912fa22ade84b1ed39c09f62d2f57ddd8758
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-06-27 11:18:31 -0400

    Update loop_agents.php

--- includes/templates/loop_agents.php
+++ includes/templates/loop_agents.php
@@ -72,9 +72,15 @@
 ?>
 
 <div class="row ns-agent-listing">
+<?php $counter = 1; ?>
 <?php if ( $agent_listing_query->have_posts() ) : while ( $agent_listing_query->have_posts() ) : $agent_listing_query->the_post(); ?>
 
     <div class="<?php echo esc_attr($agent_col_class); ?>"><?php ns_real_estate_template_loader('loop_agent.php', null, false); ?></div>
+
+    <?php 
+    if($counter % $agent_col_num == 0) { echo '<div class="clear"></div>'; } 
+    $counter++; 
+    ?>
 
 <?php endwhile; ?>
     <div class="clear"></div>
