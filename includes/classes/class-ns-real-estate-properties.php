b506e37a31437389797a8e148d85d9c35341b2c8
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-07-22 13:35:02 -0400

    Update class-ns-real-estate-properties.php

--- includes/classes/class-ns-real-estate-properties.php
+++ includes/classes/class-ns-real-estate-properties.php
@@ -14,7 +14,7 @@ class NS_Real_Estate_Properties {
 
 	public function __construct() {
 
-		// Get global settings
+		// Load admin object & settings
 		$this->admin_obj = new NS_Real_Estate_Admin();
         $settings_init = $this->admin_obj->load_settings();
         $this->global_settings = $this->admin_obj->get_settings($settings_init);
