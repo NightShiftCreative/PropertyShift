1dab01189492f4bdc9dda2197dd10f8ff5d2a144
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-08-27 22:25:49 -0400

    Update templates.php

--- includes/templates/templates.php
+++ includes/templates/templates.php
@@ -4,14 +4,14 @@
 /*  Used for core plugin and add-ons
 /*-----------------------------------------------------------------------------------*/
 function propertyshift_template_loader($template, $template_args = array(), $wrapper = true, $plugin_path = null) {
-	$theme_file = locate_template(array( 'ns-real-estate/' . $template));
+	$theme_file = locate_template(array( 'propertyshift/' . $template));
 
 	if($wrapper == true) { echo '<div class="ns-real-estate">'; }
 	if(empty($theme_file)) {
 		if(empty($plugin_path)) { $plugin_path = plugin_dir_path( __FILE__ ); }
 		include( $plugin_path . $template);
 	} else {
-		include(get_parent_theme_file_path('/ns-real-estate/'.$template));
+		include(get_parent_theme_file_path('/propertyshift/'.$template));
 	}
 	if($wrapper == true) { echo '</div>'; }
 }
@@ -22,7 +22,7 @@ function propertyshift_template_loader($template, $template_args = array(), $wra
 /*-----------------------------------------------------------------------------------*/
 function propertyshift_template_loader_single($template, $post_type, $plugin_path = null) {
 
-	$theme_file = locate_template(array( 'ns-real-estate/' . $template));
+	$theme_file = locate_template(array( 'propertyshift/' . $template));
 
 	if(is_singular($post_type)) {
 		if(empty($theme_file)) {
@@ -31,7 +31,7 @@ function propertyshift_template_loader_single($template, $post_type, $plugin_pat
 	    	include( $plugin_path . $template);
 	    	echo '</div>';
 	    } else {
-	    	include(get_parent_theme_file_path('/ns-real-estate/'.$template));
+	    	include(get_parent_theme_file_path('/propertyshift/'.$template));
 	    }
 	}
 }
@@ -40,24 +40,24 @@ function propertyshift_template_loader_single($template, $post_type, $plugin_pat
 /*-----------------------------------------------------------------------------------*/
 /*  Property Single Template
 /*-----------------------------------------------------------------------------------*/
-function ns_real_estate_template_property_single( $content ) {
+function propertyshift_template_property_single( $content ) {
 	ob_start();
 	propertyshift_template_loader_single('loop_property_single.php', 'ns-property');
     $content = $content.ob_get_clean();
     return $content;
 }
-add_filter( 'the_content', 'ns_real_estate_template_property_single', 20 );
+add_filter( 'the_content', 'propertyshift_template_property_single', 20 );
 
 
 /*-----------------------------------------------------------------------------------*/
 /*  Agent Single Template
 /*-----------------------------------------------------------------------------------*/
-function ns_real_estate_template_agent_single( $content ) {
+function propertyshift_template_agent_single( $content ) {
 	ob_start();
 	propertyshift_template_loader_single('loop_agent_single.php', 'ps-agent');
     $content = $content.ob_get_clean();
     return $content;
 }
-add_filter( 'the_content', 'ns_real_estate_template_agent_single', 20 );
+add_filter( 'the_content', 'propertyshift_template_agent_single', 20 );
 
 ?>
