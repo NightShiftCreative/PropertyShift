d5269069417332bddf4531eb98478a7fd1540ba5
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-12-10 21:27:00 -0500

    updates

--- includes/global-functions.php
+++ includes/global-functions.php
@@ -44,19 +44,19 @@ function propertyshift_col_class($cols = 3) {
     $class = '';
     switch($cols) {
         case 1:
-            $class = 'ps-listing-col ns-col-1';
+            $class = 'ps-listing-col ps-col-1';
             break;
         case 2:
-            $class = 'ps-listing-col ns-col-2'; 
+            $class = 'ps-listing-col ps-col-2'; 
             break;
         case 3:
-            $class = 'ps-listing-col ns-col-3'; 
+            $class = 'ps-listing-col ps-col-3'; 
             break;
         case 4:
-            $class = 'ps-listing-col ns-col-4';
+            $class = 'ps-listing-col ps-col-4';
             break;
         case 5:
-            $class = 'ps-listing-col ns-col-5';
+            $class = 'ps-listing-col ps-col-5';
             break;
     }
     return $class;
